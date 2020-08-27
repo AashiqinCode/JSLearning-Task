d8df7ca78e996fd1f37257afae553a663257e85c
AashiqinCode <aashiqahmed10101@gmail.com>
2020-08-27 14:25:30 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -4,38 +4,41 @@ import DenoComponent from "./Denomination";
 
 export default function App() {
   const [amount, setAmount] = useState(0);
+  const [showDeno, setDeno] = useState(false);
+  const [flag, setFlag] = useState(true);
 
   const moneyHandler = (e) => {
     setAmount(e.target.value);
   };
-
-  return (
-    <div className="App">
-      <h1 className="text-center">Alike Bank ATM</h1>
-
-      <form className="p-3">
-        <div className="form-group">
-          <label htmlFor="amount">Amount(₹)</label>
-          <input
-            type="text"
-            className="form-control"
-            id="amount"
-            placeholder="Enter Amount"
-            onChange={(e) => {
-              moneyHandler(e);
-            }}
-          />
-        </div>
-        <button
-          type="button"
-          className="btn btn-light"
-          onClick={() => {
-            return <DenoComponent amount={amount} />;
-          }}
-        >
-          Withdrawl
-        </button>
-      </form>
-    </div>
-  );
+  const moneyCheck = () => {
+    if (amount <= 0 && amount > 25000 && amount % 100 !== 0) {
+      setDeno(false);
+    } else setDeno(true);
+    setFlag(false);
+  };
+  if (flag) {
+    return (
+      <div className="App">
+        <h1 className="text-center"> Alike Bank ATM </h1>
+        <form className="p-3">
+          <div className="form-group">
+            <label htmlFor="amount"> Amount(₹) </label>{" "}
+            <input
+              type="text"
+              className="form-control"
+              id="amount"
+              placeholder="Enter Amount"
+              onChange={(e) => {
+                moneyHandler(e);
+              }}
+            />{" "}
+          </div>{" "}
+          <button type="button" className="btn btn-light" onClick={moneyCheck}>
+            Withdrawl
+          </button>
+        </form>
+      </div>
+    );
+  }
+  return <DenoComponent show={showDeno} amount={amount} />;
 }
--- src/Denomination.js
+++ src/Denomination.js
@@ -1,44 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 
 const DenoComponent = (props) => {
-  const { amount } = props;
+  const { show, amount } = props;
   const denomination = [2000, 500, 200, 100];
   const resultArray = [];
-  const [errormsg, setError] = useState("");
 
   let total = amount;
 
-  function check() {
-    if (amount < 0 && amount > 25000) {
-      setError("Invalid Amount,Enter greater than 0 and less than 25,000");
-    } else {
-      if (amount % 100 !== 0) {
-        setError("Enter valid amount");
-      } else {
-        for (let i = 0; i < denomination.length; i++) {
-          resultArray.push(Math.floor(total / denomination[i]));
-          // Get the new total
-          total = total % denomination[i]; //total=amount
-        }
-      }
+  console.log(amount,show)
+  if (show) {
+    for (let i = 0; i < denomination.length; i++) {
+      resultArray.push(Math.floor(total / denomination[i]));
+      // Get the new total
+      total = total % denomination[i]; //total=amount
     }
-  }
-
-  check();
-  if (errormsg !== null) {
     return (
       <div>
-        <h4>Denomination Fetched:</h4>
-        <div>₹2000 x {resultArray[0]}</div>
-        <div>₹500 x {resultArray[1]}</div>
-        <div>₹200 x {resultArray[2]} </div>
-        <div>₹100 x {resultArray[3]}</div>
-        <br />
-        <div>Total: {amount}</div>
+        <h4> Denomination Fetched: </h4>
+        <div> ₹2000 x {resultArray[0]} </div>{" "}
+        <div> ₹500 x {resultArray[1]} </div>{" "}
+        <div> ₹200 x {resultArray[2]} </div>{" "}
+        <div> ₹100 x {resultArray[3]} </div> <br />
+        <div> Total: {amount} </div>{" "}
       </div>
     );
   } else {
-    return <div>{errormsg}</div>;
+    return <div>Invalid Amount</div>;
   }
 };
 
