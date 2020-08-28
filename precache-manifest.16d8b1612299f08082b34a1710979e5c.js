6de42e1265ac52fc14f2517b1f471971b144f412
AashiqinCode <aashiqahmed10101@gmail.com>
2020-08-28 23:04:36 +0530

    Updates

---
+++ precache-manifest.16d8b1612299f08082b34a1710979e5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e87901989850f0c9a6bdb2cd417f80",
+    "url": "/JSLearning-Task/index.html"
+  },
+  {
+    "revision": "8a085c4ad57c9ca48c37",
+    "url": "/JSLearning-Task/static/css/main.62664bbe.chunk.css"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/JSLearning-Task/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "8a085c4ad57c9ca48c37",
+    "url": "/JSLearning-Task/static/js/main.8e466490.chunk.js"
+  },
+  {
+    "revision": "2b5313f9704306049b21",
+    "url": "/JSLearning-Task/static/js/runtime~main.adddc8e4.js"
+  }
+]);
